a95c66c988a8c7d143556c4c3ea7b39d513a6ff0
Endré Hadzalic <endre.had@gmail.com>
2021-02-02 16:45:04 +0100

    Ferding med Thread implementasjon

--- src/main/java/øvinger/en/tjener/KlientTråd.java
+++ src/main/java/øvinger/en/tjener/KlientTråd.java
@@ -9,6 +9,7 @@ public class KlientTråd extends Thread {
     private Socket socket;
     private BufferedReader klientLeser;
     private PrintWriter klientSkriver;
+    private boolean running;
 
     public KlientTråd(Socket socket, BufferedReader klientLeser, PrintWriter klientSkriver) {
         this.socket = socket;
@@ -17,51 +18,52 @@ public class KlientTråd extends Thread {
     }
 
     public void run() {
-        try {
-            final int PLUSS = 1;
-            final int MINUS = 2;
-            final int AVSLUTT = 9;
-            System.out.println("Thread" + this.getId() + "viser hovedmeny");
-            this.klientSkriver.println(" ");
-            this.klientSkriver.println("Pluss og Minus tjener Meny:");
-            this.klientSkriver.println(" ");
-            this.klientSkriver.println("1. Pluss");
-            this.klientSkriver.println("2. Minus");
-            this.klientSkriver.println("9. Koble fra");
-            this.klientSkriver.println("Skriv inn korresponderende tall for å velge handling...");
-            int menyvalg = this.mottaInt();
-            switch (menyvalg) {
-                case PLUSS:
-                    pluss();
-                    break;
-                case MINUS:
-                    minus();
-                    break;
-                case AVSLUTT:
-                    avslutt();
-                    break;
-                default:
-                    this.klientSkriver.println(menyvalg + ", er ikke et gyldig menyvalg.");
-                    break;
-            }
-        } catch (Exception e) {
-            System.out.println(this.getId() + ":" + e.toString());
-        }
+        this.running = true;
+        while (this.running) {
+            try {
 
+                final int PLUSS = 1;
+                final int MINUS = 2;
+                final int AVSLUTT = 9;
+                System.out.println("Thread" + this.getId() + "viser hovedmeny");
+                this.klientSkriver.println(" ");
+                this.klientSkriver.println("Pluss og Minus tjener Meny:");
+                this.klientSkriver.println(" ");
+                this.klientSkriver.println("1. Pluss");
+                this.klientSkriver.println("2. Minus");
+                this.klientSkriver.println("9. Koble fra");
+                this.klientSkriver.println("Skriv inn korresponderende tall for å velge handling...");
+                int menyvalg = this.mottaInt();
+                switch (menyvalg) {
+                    case PLUSS:
+                        pluss();
+                        break;
+                    case MINUS:
+                        minus();
+                        break;
+                    case AVSLUTT:
+                        avslutt();
+                        break;
+                    default:
+                        this.klientSkriver.println(menyvalg + ", er ikke et gyldig menyvalg.");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("THREAD:" + this.getId() + ":" + e.toString());
+            }
+        }
     }
 
     private void avslutt() throws Exception {
-        try {
-            this.klientSkriver.close();
-            this.klientLeser.close();
-        } catch (Exception e) {
-            throw e;
-        }
-        System.out.println("avsluttet.");
+        this.running = false;
+        this.klientSkriver.close();
+        this.klientLeser.close();
+        this.socket.close();
+        System.out.println("THREAD:" + this.getId() + "Avsluttet.");
     }
 
     private void minus() throws Exception {
-        System.out.println("klienten har valgt minus");
+        System.out.println("THREAD:" + this.getId() + ": Klienten har valgt minus");
         this.klientSkriver.println(" ");
         this.klientSkriver.println("Minus:");
         this.klientSkriver.println("Skriv inn ett tall...");
@@ -73,7 +75,7 @@ public class KlientTråd extends Thread {
     }
 
     private void pluss() throws Exception {
-        System.out.println("klienten har valgt pluss");
+        System.out.println("THREAD:" + this.getId() + ": Klienten har valgt pluss");
         this.klientSkriver.println(" ");
         this.klientSkriver.println("Pluss:");
         this.klientSkriver.println("Skriv inn ett tall...");
@@ -86,14 +88,14 @@ public class KlientTråd extends Thread {
     }
 
     private int mottaInt() throws Exception {
-        System.out.println("avventer int-input");
+        System.out.println("THREAD:" + this.getId() + "THREAD:" + this.getId() + ": Avventer int-input");
         this.klientSkriver.println("");
         String linje = this.klientLeser.readLine();
         while (!Pattern.compile("[0-9]").matcher(linje).find()) {
             linje = this.klientLeser.readLine();
             this.klientSkriver.println("Vennligst skriv inn ett tall.");
         }
-        System.out.println("fikk: " + linje + " som input.");
+        System.out.println("THREAD:" + this.getId() + ": Fikk: " + linje + " som input.");
         return Integer.parseInt(linje);
     }
 }
--- src/main/java/øvinger/en/tjener/Tjener.java
+++ src/main/java/øvinger/en/tjener/Tjener.java
@@ -8,10 +8,6 @@ import java.net.Socket;
 
 public class Tjener {
     private ServerSocket tjener;
-    private Socket forbindelse;
-    private BufferedReader klientLeser;
-    private PrintWriter klientSkriver;
-    private boolean kjører;
 
     public Tjener(int port) throws Exception {
         try {
@@ -21,107 +17,27 @@ public class Tjener {
                 ventPåForbindelse();
             }
         } catch (Exception e) {
-            System.out.println("Noe gikk feil under oppstart: " + e.toString());
+            System.out.println("MAIN: Noe gikk feil under oppstart: " + e.toString());
         }
 
     }
 
-    private void ventPåForbindelse() throws Exception {
-        Socket forbindelse = this.tjener.accept();
-        BufferedReader klientLeser = new BufferedReader
-                (new InputStreamReader(this.forbindelse.getInputStream())
-                );
-        PrintWriter klientSkriver = new PrintWriter(this.forbindelse.getOutputStream(), true);
-        this.kjører = true;
-        this.klientSkriver.println("Du har oppnådd forbindelse med: \n" + this.tjener.toString());
-        System.out.println("opnådde forbidelse med" + this.forbindelse.toString());
-        Thread tråd = new KlientTråd(forbindelse, klientLeser, klientSkriver);
-        System.out.println("MAIN: delte ut thread: " + tråd.getId() + "til klient: " + forbindelse.toString());
-        tråd.start();
-        //menu();
-    }
-/*
-    private void meny() throws Exception {
-
-        final int PLUSS = 1;
-        final int MINUS = 2;
-        final int AVSLUTT = 9;
-
-        while (this.kjører) {
-            System.out.println("viser hovedmeny");
-            this.klientSkriver.println(" ");
-            this.klientSkriver.println("Pluss og Minus tjener Meny:");
-            this.klientSkriver.println(" ");
-            this.klientSkriver.println("1. Pluss");
-            this.klientSkriver.println("2. Minus");
-            this.klientSkriver.println("9. Koble fra");
-            this.klientSkriver.println("Skriv inn korresponderende tall for å velge handling...");
-            int menyvalg = mottaInt();
-            switch (menyvalg) {
-                case PLUSS:
-                    pluss();
-                    break;
-                case MINUS:
-                    minus();
-                    break;
-                case AVSLUTT:
-                    avslutt();
-                    break;
-                default:
-                    this.klientSkriver.println(menyvalg + ", er ikke et gyldig menyvalg.");
-                    break;
-            }
-        }
-    }
-
-    private void avslutt() throws Exception {
+    private void ventPåForbindelse() {
         try {
-            this.klientSkriver.close();
-            this.klientLeser.close();
-            this.tjener.close();
-            this.kjører = false;
+            Socket forbindelse = this.tjener.accept();
+            BufferedReader klientLeser = new BufferedReader
+                    (new InputStreamReader(forbindelse.getInputStream())
+                    );
+            PrintWriter klientSkriver = new PrintWriter(forbindelse.getOutputStream(), true);
+            klientSkriver.println("Du har oppnådd forbindelse med: \n" + this.tjener.toString());
+            System.out.println("MAIN: Opnådde forbidelse med" + forbindelse.toString());
+            Thread tråd = new KlientTråd(forbindelse, klientLeser, klientSkriver);
+            System.out.println("MAIN: delte ut thread: " + tråd.getId() + "til klient: " + forbindelse.toString());
+            tråd.start();
         } catch (Exception e) {
-            throw e;
+            System.out.println(e.toString());
         }
-        System.out.println("avsluttet.");
     }
-
-    private void minus() throws Exception {
-        System.out.println("klienten har valgt minus");
-        this.klientSkriver.println(" ");
-        this.klientSkriver.println("Minus:");
-        this.klientSkriver.println("Skriv inn ett tall...");
-        int tall1 = mottaInt();
-        this.klientSkriver.println("- et tall til...");
-        int tall2 = mottaInt();
-        this.klientSkriver.println(tall1 + "-" + tall2 + "=");
-        this.klientSkriver.println(tall1 - tall2);
-    }
-
-    private void pluss() throws Exception {
-        System.out.println("klienten har valgt pluss");
-        this.klientSkriver.println(" ");
-        this.klientSkriver.println("Pluss:");
-        this.klientSkriver.println("Skriv inn ett tall...");
-        int tall1 = mottaInt();
-        this.klientSkriver.println("+ et tall til...");
-        int tall2 = mottaInt();
-        this.klientSkriver.println(tall1 + "+" + tall2 + "=");
-        this.klientSkriver.println(tall1 + tall2);
-
-    }
-
-    private int mottaInt() throws Exception {
-        System.out.println("avventer int-input");
-        this.klientSkriver.println("");
-        String linje = this.klientLeser.readLine();
-        while (!Pattern.compile("[0-9]").matcher(linje).find()) {
-            linje = this.klientLeser.readLine();
-            this.klientSkriver.println("Vennligst skriv inn ett tall.");
-        }
-        System.out.println("fikk: " + linje + " som input.");
-        return Integer.parseInt(linje);
-    }*/
 }
 
 
